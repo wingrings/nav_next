3f711ad241b23aed030fbcdc5c204dd1c4616595
卫杰 <>
2025-02-13 16:27:16 +0800

    feat: 更改axios文件为孩子

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,6 @@ import request from '@/lib/http';
 
 const prefix = '';
 
-enum apis {
-  getTableList = 'GET api query:results,page,size',
-  getUsers = 'POST user/login'
-}
-
 
 
 export const getTableList = (data: {

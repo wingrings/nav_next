b2e3384460d0c67315afa89ce0b7a2751a743767
卫杰 <>
2025-03-11 17:25:06 +0800

    feat: push

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 // import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export function middleware() {
   // const acceptLanguage = request.headers.get('accept-language');
 
   // console.log(acceptLanguage);

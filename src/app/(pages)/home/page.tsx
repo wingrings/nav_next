c00cf12af82f8670e0e20acaaafe852111bba1b4
卫杰 <>
2025-03-22 17:13:31 +0800

    push

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -7,7 +7,12 @@ export default async function Pages() {
 
   return (
     <div className="min-h-[100vh] grid grid-cols-2 p-5 bg-[#0e0e0e3d]">
-      <div>这里是一些说明</div>
+      <div
+        className="flex justify-center h-full items-center text-pink-500"
+        style={{ writingMode: "vertical-rl" }}
+      >
+        这个功能不能用，服务器资源有限，正在想办法中。。。
+      </div>
       <Client />
     </div>
   );

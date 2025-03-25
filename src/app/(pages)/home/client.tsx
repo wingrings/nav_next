f26a4e403b04dfc8d36b0eb2ea63ea3c07e8c812
卫杰 <>
2025-03-25 10:32:40 +0800

    feat:push

--- src/app/(pages)/home/client.tsx
+++ src/app/(pages)/home/client.tsx
@@ -1,52 +1,52 @@
 "use client";
-import { Textarea, Form, ScrollShadow, Avatar } from "@heroui/react";
-import { ButtonNice } from "@/components/hero";
-import React, { useState, useEffect, useRef } from "react";
+// import { Textarea, Form, ScrollShadow, Avatar } from "@heroui/react";
+// import { ButtonNice } from "@/components/hero";
+// import React, { useState, useEffect, useRef } from "react";
 
 export default function Client() {
-  const [messages, setMessages] = useState<string[]>([]);
-  const formRef = useRef<HTMLFormElement>(null);
+  // const [messages, setMessages] = useState<string[]>([]);
+  // const formRef = useRef<HTMLFormElement>(null);
 
-  useEffect(() => {
-    // 连接 SSE
-    const eventSource = new EventSource("/api/chat");
+  // useEffect(() => {
+  // 连接 SSE
+  // const eventSource = new EventSource("/api/chat");
 
-    eventSource.onmessage = (e) => {
-      setMessages((prev) => [...prev, JSON.parse(e.data)]);
-    };
+  // eventSource.onmessage = (e) => {
+  //   setMessages((prev) => [...prev, JSON.parse(e.data)]);
+  // };
 
-    return () => eventSource.close();
-  }, []);
+  // return () => eventSource.close();
+  // }, []);
 
-  const sendMessage = async (value: string) => {
-    try {
-      await fetch("/api/send", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: value }),
-      });
-      handleReset();
-    } catch (error) {
-      console.error("发送失败:", error);
-    }
-  };
+  // const sendMessage = async (value: string) => {
+  //   try {
+  //     await fetch("/api/send", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({ message: value }),
+  //     });
+  //     handleReset();
+  //   } catch (error) {
+  //     console.error("发送失败:", error);
+  //   }
+  // };
 
-  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const value = new FormData(e.currentTarget).get("value") as string;
-    sendMessage(value);
-  }
-  const handleReset = () => {
-    if (formRef.current) {
-      formRef.current.reset(); // 重置表单
-    }
-  };
+  // function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
+  //   const value = new FormData(e.currentTarget).get("value") as string;
+  //   sendMessage(value);
+  // }
+  // const handleReset = () => {
+  //   if (formRef.current) {
+  //     formRef.current.reset(); // 重置表单
+  //   }
+  // };
 
   return (
     <div className="bg-white rounded-3xl p-10 w-full mr-10">
       <div className="pb-5">
-        <ScrollShadow className="h-[50vh]">
-          <div className="flex flex-col gap-5">
+        {/* <ScrollShadow className="h-[50vh]"> */}
+        {/* <div className="flex flex-col gap-5">
             {messages.map((item, index) => {
               return (
                 <div key={index} className="pl-2 flex items-center gap-4">
@@ -64,11 +64,11 @@ export default function Client() {
                 </div>
               );
             })}
-          </div>
-        </ScrollShadow>
+          </div> */}
+        {/* </ScrollShadow> */}
       </div>
 
-      <Form ref={formRef} onSubmit={onSubmit}>
+      {/* <Form ref={formRef} onSubmit={onSubmit}>
         <Textarea
           labelPlacement="outside"
           placeholder="Enter your description"
@@ -78,7 +78,7 @@ export default function Client() {
         <ButtonNice className="w-full" size="lg" type="submit">
           发 送
         </ButtonNice>
-      </Form>
+      </Form> */}
     </div>
   );
 }
